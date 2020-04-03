9f73726d59b62e50755253341655bb26d2545e98
Sakina Shivji <shivjisakina@gmail.com>
2020-04-03 12:40:21 -0500

    added searchparams form with label + input

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,44 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  /* return React.createElement(
-        "div", {
-            id: "something",
-            class: "something",
-        },
-        [
-            React.createElement(
-                "h1", {
-                    class: "test",
-                },
-                "Adopt Me"
-            ),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese",
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel",
-            }),
-            React.createElement(Pet, {
-                name: "Doink",
-                animal: "Cat",
-                breed: "Mixed",
-            }),
-        ]
-    ); */
-
   return (
     <div>
       <h1 id="something">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed Test" />
+      <SearchParams />
     </div>
   );
 };
