52cf1833b37145e0e79bced8bb12f34c1d3d9030
Sakina Shivji <shivjisakina@gmail.com>
2020-04-03 10:36:18 -0500

    added react through npm + added imports

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,9 +1,18 @@
 //code goes here
-const Pet = (props) => {
+import React from "react";
+import {
+    render
+} from "react-dom";
+
+const Pet = ({
+    name,
+    animal,
+    breed
+}) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
     ]);
 };
 
@@ -11,31 +20,32 @@ const App = () => {
     return React.createElement(
         "div", {
             id: "something",
-            class: "something"
+            class: "something",
         },
         [
-            React.createElement("h1", {
-                class: "test"
-            }, "Adopt Me"),
+            React.createElement(
+                "h1", {
+                    class: "test",
+                },
+                "Adopt Me"
+            ),
             React.createElement(Pet, {
                 name: "Luna",
                 animal: "Dog",
-                breed: "Havanese"
+                breed: "Havanese",
             }),
             React.createElement(Pet, {
                 name: "Pepper",
                 animal: "Bird",
-                breed: "Cockatiel"
+                breed: "Cockatiel",
             }),
             React.createElement(Pet, {
                 name: "Doink",
                 animal: "Cat",
-                breed: "Mixed"
-            })
-        ]);
+                breed: "Mixed",
+            }),
+        ]
+    );
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+render(React.createElement(App), document.getElementById("root"));
