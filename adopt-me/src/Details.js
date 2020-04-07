ef7829a7cb045bf1108de4a040e1c322af211f41
Sakina Shivji <shivjisakina@gmail.com>
2020-04-07 11:58:28 -0500

    updated details page for each pet

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,11 +1,50 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  // similar to useEffect aka runs when it first starts up then stops and doesnt rerun
+  // helpful for API requests
+  componentDidMount() {
+    // Props are read only, cant change them (info from parents that gets handed down through params)
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        // stop loading after data comes back from the API
+        loading: false,
+      });
+    }, console.error);
+  }
+  // Class components wont work without render method, cant use hooks with classes i.e useState
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
